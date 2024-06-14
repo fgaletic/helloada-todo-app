76e39235e069dbf612bf93c435d86ecbd464d0b9
Filip Galetic <70514954+fgaletic@users.noreply.github.com>
2024-06-14 18:05:33 +0200

    chore: Update route for todoRouter in backend/index.js

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ mongoose.connect(mongoURI)
   .catch((err) => console.error('MongoDB connection error:', err));
 
 
-app.use('/todos', todoRouter);
+app.use('/', todoRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
